ffb1d6b895bb81e7a88332a4820b7aeab1f66fbb
vedant3114 <157204994+vedant3114@users.noreply.github.com>
2025-06-21 17:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 56.9 MB (9.14%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,19 +1,21 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-
-         int max=arr[0];
-
-        for(int i=1;i<arr.length;i++){
-            if (arr[i]>max){
-                max=arr[i];
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=(start+end)/2;
+            if(arr[mid]>arr[mid+1]){
+                // this condition is true when we are decreasing side of array
+                //in this  scenario end miht be the max element
+                end=mid;
+            }
+            else{
+                //this part is executed for increasing side
+                start=mid+1;
             }
         }
-        for(int i=0;i<arr.length;i++){
-            if (arr[i]==max){
-                return i;
-            }
-        }
-        return 0;
+        //at the end start and end will be at same  position pointing towards the mmax element
+        return end;
         
         
     }
