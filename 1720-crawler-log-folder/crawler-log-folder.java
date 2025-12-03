5730fc1f813faf944dbc309d2f3e0ae0d0a87bf8
VEDANT SHIMPI <157204994+vedant3114@users.noreply.github.com>
2025-12-03 11:32:17 +0530

    Time: 2 ms (35.31%) | Memory: 43.8 MB (29.99%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minOperations(String[] logs) {
         Stack<String>stack =new Stack<>();
-        int count=0;
         for(String i:logs){
             if(i.equals("../")){
                 if (!stack.isEmpty()) {
