ab0c0733e2ec136166dd8fd4af31144572e47507
VEDANT SHIMPI <157204994+vedant3114@users.noreply.github.com>
2025-12-06 11:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (55.61%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int og_sum=0;
+        int left=0;
+        int right=0;
+        for(int i=0;i<nums.length;i++){
+            og_sum+=nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            right=og_sum-left-nums[i];
+            if(left==right){
+                return i;
+            }
+            left+=nums[i];
+        }
+        return -1;
+
+        
+    }
+}
