1d270caac0e3c031d48135961200377a2b2e998b
VEDANT SHIMPI <157204994+vedant3114@users.noreply.github.com>
2025-12-07 19:16:43 +0530

    Time: 1428 ms (5.06%) | Memory: 42 MB (21.38%) - LeetSync

---
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int count=0;
+        while(low<=high){
+            if((low & 1)!=0){
+                count++;
+            }
+            low++;
+        }
+        return count;
+        
+    }
+}
